a8c1ca66d6c3054a50e6b89e6068977f07da9938
Zachery Pipes <164957446+ZachPipes@users.noreply.github.com>
2026-02-04 21:47:30 -0600

    Initial commit

---
+++ Faraday/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Faraday;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application { }
